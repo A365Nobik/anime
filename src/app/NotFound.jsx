0d8d706bea3be4c957a460a3fdb103a9978fb1ea
A365Nobik <matvey.yarikov@gmail.com>
2025-06-04 20:19:11 +0300

    vercel test

--- src/app/NotFound.jsx
+++ src/app/NotFound.jsx
@@ -3,7 +3,9 @@ export default function NotFound() {
     <>
       <div className="flex flex-col justify-center items-center h-screen">
         <h1 className="text-white text-5xl font-medium">Page Not Found!</h1>
-        <a className="text-4xl text-orange-500 " href="/">Home Page</a>
+        <a className="text-4xl text-orange-500 " href="/">
+          Home Page
+        </a>
       </div>
     </>
   );
