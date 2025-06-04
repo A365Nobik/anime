3cdc8df5bf0cd52c5979edec5ad3eeffea304137
A365Nobik <matvey.yarikov@gmail.com>
2025-06-04 20:10:14 +0300

    routes new path

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/result" element={<SearchResult />}></Route>
-      <Route path="/info" element={<AnimeInfo />}></Route>
+      <Route path="/result*" element={<SearchResult />}></Route>
+      <Route path="/info*" element={<AnimeInfo />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
